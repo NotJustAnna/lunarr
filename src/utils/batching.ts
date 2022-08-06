9132d6eca4832171cce8c369db62d2d71089aba0
Adrian Todt Monteiro da Silva <adriantodt.ms@gmail.com>
2022-08-06 18:59:25 -0300

    Fixed a diff bug

--- src/utils/batching.ts
+++ src/utils/batching.ts
@@ -3,17 +3,17 @@ export class BatchingUtil<T> {
   private steal?: () => void;
 
   constructor(
-    public maxTimeBatching: number,
+    public maxTimeWaiting: number,
     public maxBatchSize: number,
   ) {
-    if (maxTimeBatching < 0) {
-      throw new Error('maxTimeBatching must be a non-negative number.');
+    if (maxTimeWaiting < 0) {
+      throw new Error('maxTimeWaiting must be a non-negative number.');
     }
     if (maxBatchSize < 0) {
       throw new Error('maxBatchSize must be a non-negative number.');
     }
-    if (maxTimeBatching === 0 && maxBatchSize === 0) {
-      throw new Error('Either maxTimeBatching or maxBatchSize must be non-zero.');
+    if (maxTimeWaiting === 0 && maxBatchSize === 0) {
+      throw new Error('Either maxTimeWaiting or maxBatchSize must be non-zero.');
     }
   }
 
@@ -23,14 +23,17 @@ export class BatchingUtil<T> {
    */
   async aggregate(item: T): Promise<T[] | undefined> {
     this.bucket.push(item);
-    if (this.bucket.length >= this.maxBatchSize) {
+    if (this.maxBatchSize !== 0 && this.bucket.length >= this.maxBatchSize) {
       const batch = this.bucket;
       this.bucket = [];
       return batch;
     }
-    if (this.steal) {
-      // steal timer
-      this.steal();
+    if (this.maxTimeWaiting !== 0) {
+      if (this.steal) {
+        // steal timer
+        this.steal();
+        delete this.steal;
+      }
       let alreadyStolen = false;
       this.steal = () => {
         alreadyStolen = true;
@@ -40,15 +43,29 @@ export class BatchingUtil<T> {
           resolve(undefined);
         }
         const timer = setTimeout(() => {
+          delete this.steal;
           const batch = this.bucket;
           this.bucket = [];
           resolve(batch);
-        });
+        }, this.maxTimeWaiting);
         this.steal = () => {
           resolve(undefined);
           clearTimeout(timer);
         };
       });
     }
+  }
+
+  /**
+   * This method lets you steal the current batch of events being aggregated.
+   */
+  stealImmediately(): T[] {
+    if (this.steal) {
+      this.steal();
+      delete this.steal;
+    }
+    const batch = this.bucket;
+    this.bucket = [];
+    return batch;
   }
 }
--- src/utils/microDiff.ts
+++ src/utils/microDiff.ts
@@ -9,13 +9,19 @@ export function microDiff<T>(previous: T, changeSet: Partial<T>): Diff<T> | null
   const diff: Diff<T> = { previous, old: {}, new: {}, applied: { ...previous, ...changeSet } };
 
   for (const key in changeSet) {
-    if (changeSet[key] !== previous[key]) {
-      diff.old[key] = previous[key];
-      diff.new[key] = changeSet[key];
+    const newValue = changeSet[key], oldValue = previous[key];
+    if (newValue !== oldValue && !falsePositive(newValue, oldValue)) {
+      diff.old[key] = oldValue;
+      diff.new[key] = newValue;
     }
   }
   if (Object.keys(diff.old).length === 0 && Object.keys(diff.new).length === 0) {
     return null;
   }
   return diff;
+}
+
+function falsePositive(left: any, right: any): boolean {
+  // Thanks, JS! I hate you too.
+  return (left === undefined || left === null) && (right === undefined || right === null);
 }
