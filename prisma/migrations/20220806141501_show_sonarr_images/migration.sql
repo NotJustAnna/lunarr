25220665f8d66a5832003f8e352315ba4e26f0e7
Anna Silva <6955035+NotJustAnna@users.noreply.github.com>
2022-08-06 11:50:35 -0300

    Support for Sonarr artwork

---
+++ prisma/migrations/20220806141501_show_sonarr_images/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Show"
+    ADD COLUMN "show_sonarr_banner_image" TEXT,
+ADD COLUMN     "show_sonarr_fanart_image" TEXT,
+ADD COLUMN     "show_sonarr_headshot_image" TEXT,
+ADD COLUMN     "show_sonarr_poster_image" TEXT,
+ADD COLUMN     "show_sonarr_screenshot_image" TEXT;
